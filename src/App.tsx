d076e9ac99c2811a12de04d7bb909b1363f8d25b
Namhehe237 <namnguyen23072003@gmail.com>
2024-11-18 22:50:15 +0700

    searchbook

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { HomePage } from './layouts/HomePage/HomePage';
+import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+
+function App() {
+  return (
+      <div>
+        {/* <HomePage/> */}
+        <SearchBooksPage />
+      </div>
+  );
+}
+
+export default App;
