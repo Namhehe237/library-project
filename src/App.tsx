47efac406d832ac4f0de33db9f30cf29c357838c
Namhehe237 <namnguyen23072003@gmail.com>
2024-11-30 23:09:11 +0700

    config okta and adjust UI for login logout

--- src/App.tsx
+++ src/App.tsx
@@ -22,31 +22,35 @@ function App() {
 
   const restoreOriginalUri = async (_oktaAuth: any, originalUri: any) => {
     history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
-  }
+  };
+
 
   return (
     <div className='d-flex flex-column min-vh-100'>
       <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri} onAuthRequired={customAuthHandler}>
-        <NavBar />
-        <div className='flex-grow-1'>
-          <Switch>
-            <Route path="/" exact>
-              <Redirect to="/home" />
-            </Route>
-            <Route path='/home'>
-              <HomePage />
-            </Route>
-            <Route path='/search'>
-              <SearchBooksPage />
-            </Route>
-            <Route path="/checkout/:bookId">
-              <BookCheckoutPage />
-            </Route>
-            <Route path='/login' render={() => <LoginWidget config={oktaConfig} />}></Route>
-            <Route path='/login/callback' component={LoginCallback}> </Route>
-          </Switch>
-        </div>
-        <Footer />
+      <NavBar />
+      <div className='flex-grow-1'>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/home' />
+          </Route>
+          <Route path='/home'>
+            <HomePage />
+          </Route>
+          <Route path='/search'>
+            <SearchBooksPage />
+          </Route>
+          <Route path='/checkout/:bookId'>
+            <BookCheckoutPage/>
+          </Route>
+          <Route path='/login' render={
+            () => <LoginWidget config={oktaConfig} /> 
+            } 
+          />
+          <Route path='/login/callback' component={LoginCallback} />
+        </Switch>
+      </div>
+      <Footer />
       </Security>
     </div>
   );
--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
-    clientId: '0oalh4ujf8XUfgNdF5d7',
+    clientId: '0oaligvd28r82HawU5d7',
     issuer: 'https://dev-07213562.okta.com/oauth2/default',
-    redirectUri: 'https://localhost:3000/login/callback',
+    redirectUri: 'http://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
